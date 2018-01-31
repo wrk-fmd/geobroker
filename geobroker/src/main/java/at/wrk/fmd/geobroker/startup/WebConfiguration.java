e528f2b067b693624292251839e88e5b6e6327c1
Robert Wittek <r.m.wittek@gmail.com>
2018-01-31 19:12:48 +0100

    add example for static resource via folder

--- geobroker/src/main/java/at/wrk/fmd/geobroker/startup/WebConfiguration.java
+++ geobroker/src/main/java/at/wrk/fmd/geobroker/startup/WebConfiguration.java
@@ -3,6 +3,7 @@ package at.wrk.fmd.geobroker.startup;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
@@ -12,5 +13,10 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void configureMessageConverters(final List<HttpMessageConverter<?>> converters) {
         converters.add(new GsonHttpMessageConverter());
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 }
