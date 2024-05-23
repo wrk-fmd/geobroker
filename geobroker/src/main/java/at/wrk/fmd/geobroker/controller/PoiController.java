3df01087ca8d94c69d1232d7149f3d5554213243
Robert Wittek <r.m.wittek@gmail.com>
2024-05-23 19:04:02 +0200

    upgrade to spring boot 3.x

--- geobroker/src/main/java/at/wrk/fmd/geobroker/controller/PoiController.java
+++ geobroker/src/main/java/at/wrk/fmd/geobroker/controller/PoiController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import java.io.Serializable;
 import java.util.Optional;
 import java.util.Set;
